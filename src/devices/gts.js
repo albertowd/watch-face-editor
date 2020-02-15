41912b9375b2cb44469fdebaa269f1af7b3da132
Alberto Wollmann Dietrich <alberto+git@albertowd.com.br>
2020-02-15 12:38:05 -0300

    Adding GTS initial specs.

---
+++ src/devices/gts.js
@@ -0,0 +1,21 @@
+import defaultGTS from './defaults/gts';
+import functionsGTS from './functions/gts'
+
+const gts = {
+  defaults: defaultGTS,
+  dimensions: {
+    height: 442,
+    width: 348
+  },
+  functions: functionsGTS,
+  name: 'gts',
+  preview: {
+    bottom: 223,
+    letf: 62,
+    right: 92,
+    top: 223,
+    zoom: 1.0
+  }
+};
+
+export default gts;
