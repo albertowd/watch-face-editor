595b8c0ecd7072f5802a3991d979797fa8351143
Alberto Dietrich <alberto+git@albertowd.com.br>
2020-03-20 16:43:58 -0300

    weather example

--- store/weather.js
+++ store/weather.js
@@ -1,4 +1,51 @@
 export const state = () => ({
+  icon: {
+    images: {
+      images: [],
+      x: 0,
+      y: 0
+    },
+    noWeatherImage: null
+  },
+  temperature: {
+    current: {
+      alignment: 'Center',
+      bottom: 245,
+      images: [],
+      left: 15,
+      right: 115,
+      spacing: 0,
+      top: 205
+    },
+    symbols: {
+      degreesImage: null,
+      minusImage: null,
+      noDataImage: null
+    },
+    today: {
+      separate: {
+        appendDegress: true,
+        day: {
+          alignment: 'Center',
+          bottom: 245,
+          images: [],
+          left: 15,
+          right: 115,
+          spacing: 0,
+          top: 205
+        },
+        night: {
+          alignment: 'Center',
+          bottom: 245,
+          images: [],
+          left: 15,
+          right: 115,
+          spacing: 0,
+          top: 205
+        }
+      }
+    }
+  }
 })
 
 export const mutations = {
